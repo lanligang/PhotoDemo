f1335c49d16bcdd22dd869512365b8883a56346f
LenSky <1176281703@qq.com>
2019-04-02 15:34:55 +0800

    PhotoDemo

--- PhotoDemo/AppDelegate.h
+++ PhotoDemo/AppDelegate.h
@@ -5,7 +5,6 @@
 //  Created by ios2 on 2019/3/1.
 //  Copyright © 2019 ShanZhou. All rights reserved.
 //
-//这你的文件要修改了 注意一下
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
